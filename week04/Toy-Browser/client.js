2505d62e3da4c30a241c4db05eca6407ac56644b
lisuting <lisuting@founq.com>
2020-08-02 23:53:31 +0800

    update

--- week04/Toy-Browser/client.js
+++ week04/Toy-Browser/client.js
@@ -1,17 +1,42 @@
 // URL解析HTTP并响应
 const net = require('net')
 const parser = require('./parser.js')
+const { resolve } = require('path')
 
-
+// request类
 class Request{
     // 对请求进行一定的处理
+    // 储存传入进来的值
     constructor(options){
-        this.method = options.method || 'GET'
-        this.host = options.host
-        this.port = options.port || 80
-        this.path = options.path || "/"
-        this.body = options.body || {}
+        this.method = options.method || 'GET' // 请求方式
+        this.host = options.host //请求接口
+        this.port = options.port || 80 //请求端口
+        this.path = options.path || "/" // 请求路由
+        this.body = options.body || {} //请求体
         this.headers = options.headers || {}
+        // request中一定要求有content-type
+        /* 常见的媒体格式类型如下：
+
+            text/html ： HTML格式
+            text/plain ：纯文本格式      
+            text/xml ：  XML格式
+            image/gif ：gif图片格式    
+            image/jpeg ：jpg图片格式 
+            image/png：png图片格式
+            以application开头的媒体格式类型：
+
+            application/xhtml+xml ：XHTML格式
+            application/xml     ： XML数据格式
+            application/atom+xml  ：Atom XML聚合格式    
+            application/json    ： JSON数据格式（最常用）
+            application/pdf       ：pdf格式  
+            application/msword  ： Word文档格式
+            application/octet-stream ： 二进制流数据（如常见的文件下载）
+            application/x-www-form-urlencoded ： <form encType=””>中默认的encType，form表单数据被编码为key/value格式发送到服务器（表单默认的提交数据的格式）
+            另外一种常见的媒体格式是上传文件之时使用的：
+
+            multipart/form-data ： 需要在表单中进行文件上传时，就需要使用该格式
+             */
         if(!this.headers["Content-Type"]){
             this.headers["Content-Type"] = "application/x-www-form-urlencoded"
         }
@@ -19,16 +44,30 @@ class Request{
         if(this.headers["Content-Type"] === "application/json"){
             this.bodyText = JSON.stringify(this.body)
         }else if(this.headers["Content-Type"] === "application/x-www-form-urlencoded"){
+            // 将{name:'suky'}这样的对象格式转化成 name="suky"&...的格式
+            // encodeURIComponent 可把字符串作为 URI 组件进行编码(解析成服务器读得懂的形式)
             this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&')
+        }else if(this.headers["Content-Type"] === 'text/plain'){ // 嘻嘻 自己加的试试看
+            // 400报错，应该是node端没有对于这个请求的解析器
+            this.bodyText = this.body
         }
 
-        this.headers["Content-Length"] = this.bodyText.length
+        this.headers["Content-Length"] = this.bodyText.length // 请求体长度
     }
 
     // 建立链接并发送请求
     send(connection){
+        // step 2 send our request
+        // 建立TCP链接应该是流传递，为啥会有Promise
+        // 逐步接受并构建，直到构建完成时才会通过Promise返回，这时候就从流传输变为点
+        // return new Promise((resolev, reject) => {
+        //     console.log('success')
+        //     resolve()
+        // })
+
         return new Promise((resolve, reject) => {
-            const parser = new ResponseParser() // 接受的是流模式
+            const parser = new ResponseParser() // 接受的是流模式，会将接受到的信息逐步传入进行处理
+            // 进行TCP链接
             if(connection){
                 console.log('已有连接');
                 // 建立链接的话，数据写进去传出去
@@ -40,21 +79,25 @@ class Request{
                     port: this.port
                 }, () => {
                     console.log('连接到服务器')
-                    connection.write(this.toString())
+                    connection.write(this.makeRequestHead())
                     // console.log(this.toString())
                 });
             }
 
             // 流模式，持续接受参数
-            connection.on('data', function(data){
+            connection.on('data', (data) => {
+                console.log('收到响应')
+                // 响应也是二进制的Buffer类型,将二进制数据转化为字符串，与this.toString()并不一致
                 // console.log(data.toString())
+
+                // 送到响应状态机进行解析
                 parser.receive(data.toString())
                 
-                if(parser.isFinished){
-                    console.log('解析完成')
-                    resolve(parser.response)
-                    connection.end() // 关闭链接
-                }
+                // if(parser.isFinished){
+                //     console.log('解析完成')
+                //     resolve(parser.response)
+                //     connection.end() // 关闭链接
+                // }
             });
 
             connection.on('error', (err) => {
@@ -65,7 +108,21 @@ class Request{
         })
     }
 
-    toString(){
+    // 解析我们的响应文件成为以下的文件
+    /*
+    *   HTTP/1.1 200 OK
+        Content-Type: text/html
+        Date: Sun, 02 Aug 2020 13:21:12 GMT
+        Connection: keep-alive
+        Transfer-Encoding: chunked
+
+        c
+        Hello world
+
+        0
+    */
+   // 传输过去构造请求头的样式
+    makeRequestHead(){
         return `${this.method} ${this.path} HTTP/1.1\r
 ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r
 \r
@@ -73,40 +130,44 @@ ${this.bodyText}`
     }
 }
 
+// step 3 construct a response parser to recive data step by step
 class ResponseParser{
     constructor(){
-        this.WAITING_STATUS_LINE = 0
-        this.WAITING_STATUS_LINE_END = 1
-        this.WAITING_HEADER_NAME = 2
-        this.WAITING_HEADER_SPACE = 3
-        this.WAITING_HEADER_VALUE = 4
-        this.WAITING_HEADER_LINE_END = 5
-        this.WAITING_HEADER_BLOCK_END = 6
-        this.WAITING_BODY = 7
+        this.WAITING_STATUS_LINE = 'WAITING_STATUS_LINE' //请求行 HTTP/1.1 200 OK 得到我们的状态码
+        this.WAITING_STATUS_LINE_END = 'WAITING_STATUS_LINE_END' // 请求行 解析完成后的状态
+        this.WAITING_HEADER_NAME = 'WAITING_HEADER_NAME'
+        this.WAITING_HEADER_SPACE = 'WAITING_HEADER_SPACE'
+        this.WAITING_HEADER_VALUE = 'WAITING_HEADER_VALUE'
+        this.WAITING_HEADER_LINE_END = 'WAITING_HEADER_LINE_END'
+        this.WAITING_HEADER_BLOCK_END = 'WAITING_HEADER_BLOCK_END'
+        this.WAITING_BODY = 'WAITING_BODY'
 
         this.current = this.WAITING_STATUS_LINE  // 当前状态 
-        this.statusLine = ""
-        this.headers = {}
-        this.headerName = ""
-        this.headerValue = ""
-        this.bodyParser = null
+        this.statusLine = "" // 储存状态码的哪一行
+        // this.headers = {}
+        // this.headerName = ""
+        // this.headerValue = ""
+        // this.bodyParser = null
     }
 
-    get isFinished(){
-        return this.bodyParser && this.bodyParser.isFinished
-    }
+    // get isFinished(){
+    //     return this.bodyParser && this.bodyParser.isFinished
+    // }
 
-    get response(){
-        this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
-        return {
-            statusCode: RegExp.$1,
-            statusText: RegExp.$2,
-            headers: this.headers,
-            body: this.bodyParser.content.join('')
-        }
-    }
+    // get response(){
+    //     this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+    //     return {
+    //         statusCode: RegExp.$1,
+    //         statusText: RegExp.$2,
+    //         headers: this.headers,
+    //         body: this.bodyParser.content.join('')
+    //     }
+    // }
 
+    // 响应值传入，传入时进行状态机的处理
     receive(string){
+        console.log(string)
+        // 逐个字符解析 
         for(let i=0;i<string.length;i++){
             this.receiveChar(string.charAt(i))
         }
@@ -114,51 +175,57 @@ class ResponseParser{
 
     // 对响应进行状态解析
     receiveChar(char){
-        if(this.current === this.WAITING_STATUS_LINE){ //请求行 HTTP/1.1 200 OK
-            if(char === '\r'){ // 等到换行时进入下一个状态
-                this.current = this.WAITING_STATUS_LINE_END
-            }else{
-                this.statusLine += char
-            }
-        }else if(this.current === this.WAITING_STATUS_LINE_END){ // HTTP/1.1 200 OK 的结尾
-            if(char === '\n'){
-                this.current = this.WAITING_HEADER_NAME // 响应头 Content-Type: text/html
-            }
-        }else if(this.current === this.WAITING_HEADER_NAME){
-            if(char === ':'){ // Content-Type: text/html  Content-Type: 后面跟着一个空格
-                this.current = this.WAITING_HEADER_SPACE 
-            }else if(char === '\r'){
-                this.current = this.WAITING_HEADER_BLOCK_END // 响应头结束
-                // 和head 相关没法一开始就能够建立
-                if(this.headers['Transfer-Encoding'] === 'chunked'){
-                    this.bodyParser = new TrunkedBodyParser()
+        // console.log(char)
+        switch(this.current){
+            case this.WAITING_STATUS_LINE://响应行 HTTP/1.1 200 OK
+                if(char === '\r'){ // 等到换行时进入下一个状态
+                    this.current = this.WAITING_STATUS_LINE_END
+                }else{
+                    this.statusLine += char
                 }
-            }else{
-                this.headerName += char // text/html
-            }
-        }else if(this.current === this.WAITING_HEADER_SPACE){
-            if(char === ' '){
-                this.current = this.WAITING_HEADER_VALUE //Content-Type: 后面跟着一个空格跳到这里
-            }
-        }else if(this.current === this.WAITING_HEADER_VALUE){
-            if(char === '\r'){ // this.headerValue 结束后
-                this.current = this.WAITING_HEADER_LINE_END
-                this.headers[this.headerName] = this.headerValue
-                this.headerName = "" // zhi kong
-                this.headerValue = ""
-            }else{
-                this.headerValue += char
-            }
-        }else if(this.current === this.WAITING_HEADER_LINE_END){ 
-            if(char === '\n'){
-                this.current = this.WAITING_HEADER_NAME // Content-Type 读完
-            }
-        }else if(this.current === this.WAITING_HEADER_BLOCK_END){
-            if(char === '\n'){
-                this.current = this.WAITING_BODY
-            }
-        }else if(this.current === this.WAITING_BODY){
-             this.bodyParser.receiveChar(char)
+                break;
+            case this.WAITING_STATUS_LINE_END: 
+        }
+       
+    //     }else if(this.current === this.WAITING_STATUS_LINE_END){ // HTTP/1.1 200 OK 的结尾
+    //         if(char === '\n'){
+    //             this.current = this.WAITING_HEADER_NAME // 响应头 Content-Type: text/html
+    //         }
+    //     }else if(this.current === this.WAITING_HEADER_NAME){
+    //         if(char === ':'){ // Content-Type: text/html  Content-Type: 后面跟着一个空格
+    //             this.current = this.WAITING_HEADER_SPACE 
+    //         }else if(char === '\r'){
+    //             this.current = this.WAITING_HEADER_BLOCK_END // 响应头结束
+    //             // 和head 相关没法一开始就能够建立
+    //             if(this.headers['Transfer-Encoding'] === 'chunked'){
+    //                 this.bodyParser = new TrunkedBodyParser()
+    //             }
+    //         }else{
+    //             this.headerName += char // text/html
+    //         }
+    //     }else if(this.current === this.WAITING_HEADER_SPACE){
+    //         if(char === ' '){
+    //             this.current = this.WAITING_HEADER_VALUE //Content-Type: 后面跟着一个空格跳到这里
+    //         }
+    //     }else if(this.current === this.WAITING_HEADER_VALUE){
+    //         if(char === '\r'){ // this.headerValue 结束后
+    //             this.current = this.WAITING_HEADER_LINE_END
+    //             this.headers[this.headerName] = this.headerValue
+    //             this.headerName = "" // zhi kong
+    //             this.headerValue = ""
+    //         }else{
+    //             this.headerValue += char
+    //         }
+    //     }else if(this.current === this.WAITING_HEADER_LINE_END){ 
+    //         if(char === '\n'){
+    //             this.current = this.WAITING_HEADER_NAME // Content-Type 读完
+    //         }
+    //     }else if(this.current === this.WAITING_HEADER_BLOCK_END){
+    //         if(char === '\n'){
+    //             this.current = this.WAITING_BODY
+    //         }
+    //     }else if(this.current === this.WAITING_BODY){
+    //          this.bodyParser.receiveChar(char)
         }
     }
 }
@@ -211,15 +278,19 @@ class TrunkedBodyParser{
 }
 
 
+//如何使用来设计我们的请求函数，构造一个http请求
 void async function(){ // 自执行函数
     let request = new Request({
         method: "POST",
         host: "127.0.0.1",
         port: "8088",
         path: "/",
+        // headers有哪些种类呢？
         headers:{
-            ["X-Foo2"]:"customed"
+            ["made"]:"suky own",
+            // ["Content-Type"]:"text/plain"
         },
+        // 发送的内容
         body: {
             name:"suky"
         }
@@ -228,7 +299,7 @@ void async function(){ // 自执行函数
     let response = await request.send()
 
     // 应该变成异步分段处理的
-    let dom = parser.parseHTML(response.body)  
-    console.log(dom)
+    // let dom = parser.parseHTML(response.body)  
+    // console.log(dom)
 }();
 
--- week04/Toy-Browser/server.js
+++ week04/Toy-Browser/server.js
@@ -1,17 +1,30 @@
 const http = require('http')
 
 http.createServer((request, response) => {
+    console.log('we had received a request')
     let body = []
     request.on('error', (err) =>{
         console.error(err)
-    }).on('data', (chunk) => {
-        body.push(chunk.toString())
+    }).on('data', (chunk) => { 
+        /* chunked body 
+         */
+        // console.log(chunk)
+        // chunk 是储存 Buffer类型的TCP流文件 
+        body.push(chunk.toString()) // 将二进制文件转化为string
     }).on('end', ()=>{
         // body = Buffer.concat(body).toString()
         // console.log(body)
         // console.log('body:', body)
+        console.log('响应')
         response.writeHead(200, {'Content-Type': 'text/html'})
-        response.end('<html><div>hello</div></html> ')
+        response.end(`<html>
+<head></head>
+<body>
+<div>Hello World</div>
+</body>
+</html>`)
+        console.log('结束')
+        
         // response.end(`<html>
         // <div>
         //     我的妈呀
